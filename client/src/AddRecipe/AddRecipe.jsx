572edc02c4086986da73deea2e93561615a2f20d
jennieann <mail@jennie-ann.se>
2020-09-25 17:06:29 +0200

    Convert from class to functional component

--- client/src/AddRecipe/AddRecipe.jsx
+++ client/src/AddRecipe/AddRecipe.jsx
@@ -1,77 +1,78 @@
-import React, { Component } from "react"
+import React, { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { checkLoggedIn, getCategories } from "../helpers.js"
 import styles from "./AddRecipe.module.css"
 import classnames from "classnames"
 
-class AddRecipe extends Component {
-  state = {
-    user: null,
-    recipe: { name: "" },
-    subCategories: [],
-    categories: [],
-    errorMessage: ""
-  }
-  constructor() {
-    super()
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+function AddRecipe() {
+  const [user, setUser] = useState(null)
+  const [recipe, setRecipe] = useState({ name: "" })
+  const [subCategories, setSubCategories] = useState([])
+  const [categories, setCategories] = useState([])
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const getRecipeID = () => {
+    const recipeId = window.location.href.split("/")[5]
+    return recipeId
   }
 
-  async getRecipeAsync(recipeId) {
+  const getSubCategories = async () => {
+    let response = await fetch("/api/subcategories")
+    let subCategories = await response.json()
+    return subCategories
+  }
+
+  const getUser = async () => {
+    let response = await fetch("/api/user")
+    let user = await response.json()
+    return user
+  }
+
+  useEffect(() => {
+    getCategories().then(data => {
+      setCategories(data)
+    })
+  }, [])
+
+  if (getRecipeID()) {
+    useEffect(() => {
+      getRecipeAsync(getRecipeID()).then(data => {
+        setRecipe(data.data)
+      })
+    }, [])
+  }
+
+  useEffect(() => {
+    getUser().then(user => setUser(user.user))
+  }, [])
+
+  useEffect(() => {
+    getSubCategories().then(data => {
+      setSubCategories(data.data)
+    })
+  }, [])
+
+  const getRecipeAsync = async recipeId => {
     let response = await fetch(`/api/recipes/${recipeId}`)
     let data = await response.json()
 
     return data
   }
 
-  async getUser() {
-    let response = await fetch("/api/user")
-    let user = await response.json()
-    return user
-  }
-
-  async getSubCategories() {
-    let response = await fetch("/api/subcategories")
-    let subCategories = await response.json()
-    return subCategories
-  }
-
-  componentWillMount() {
-    getCategories().then(categories => {
-      this.setState({ categories: categories })
-    })
-
-    if (this.getRecipeID()) {
-      this.getRecipeAsync(this.getRecipeID()).then(data => {
-        this.setState({ recipe: data.data })
-      })
-    }
-    this.getUser().then(user => this.setState({ user: user.user }))
-
-    this.getSubCategories().then(data => {
-      this.setState({ subCategories: data.data })
-    })
-  }
-
-  getRecipeID() {
-    const recipeId = window.location.href.split("/")[5]
-    return recipeId
-  }
-
-  isRecipeNameSet() {
+  const isRecipeNameSet = () => {
     const recipeNameInputValue =
       document.forms["addRecipe"].elements["name"].value
+
     return recipeNameInputValue === "" ? false : true
   }
 
-  setErrorMessage() {
-    this.isNameSet()
-      ? this.setState({ errorMessage: "" })
-      : this.setState({ errorMessage: "Du måste fylla i ett namn" })
+  const ErrorMessage = () => {
+    isRecipeNameSet()
+      ? setErrorMessage("")
+      : setErrorMessage("Du måste fylla i ett namn")
   }
 
-  saveRecipe(recipeId, data) {
+  const saveRecipe = (recipeId, data) => {
     if (recipeId) {
       fetch(`/api/recipes/${recipeId}`, {
         method: "PUT",
@@ -85,21 +86,19 @@ class AddRecipe extends Component {
     }
   }
 
-  handleSubmit(event) {
+  const handleSubmit = event => {
     event.preventDefault()
     const data = new FormData(event.target)
 
-    if (this.isRecipeNameSet()) {
-      this.setErrorMessage()
-      this.saveRecipe(this.getRecipeID(), data)
+    if (isRecipeNameSet()) {
+      ErrorMessage()
+      saveRecipe(getRecipeID(), data)
     } else {
-      this.setErrorMessage()
+      ErrorMessage()
     }
   }
 
-  renderSubCategoryOptions() {
-    const subCategories = this.state.subCategories
-    const categories = this.state.categories
+  const renderSubCategoryOptions = () => {
     const subCategoryOptions = []
 
     subCategories.forEach(function(subCategoryItem) {
@@ -117,11 +116,10 @@ class AddRecipe extends Component {
     return subCategoryOptions
   }
 
-  renderCategoryOption() {
-    const cats = this.state.categories
+  const renderCategoryOption = () => {
     const categoryOptions = []
 
-    cats.forEach(function(categoryItem) {
+    categories.forEach(function(categoryItem) {
       categoryOptions.push(
         <option value={categoryItem.id} key={categoryItem.id}>
           {categoryItem.name}
@@ -131,147 +129,129 @@ class AddRecipe extends Component {
     return categoryOptions
   }
 
-  handleChange(event) {
-    this.setState({
-      recipe: { ...this.state.recipe, [event.target.name]: event.target.value }
-    })
+  const handleChange = event => {
+    const { name, value } = event.target
+
+    setRecipe({ ...recipe, [name]: value })
   }
 
-  render() {
-    const isLoggedIn = checkLoggedIn(this.state.user)
-
-    const recipe = this.state.recipe
-
-    const errorMessage = this.state.errorMessage
-
-    const {
-      ingredients,
-      time_cooking,
-      description,
-      name,
-      categories_id,
-      difficulty,
-      sub_category_id
-    } = recipe
-
-    return (
-      <div className={styles.recipeWrapper}>
-        {isLoggedIn ? (
-          <div className={styles.formWrapper}>
-            <div
-              className={classnames(styles.errorMessage, {
-                [styles.active]: errorMessage !== ""
-              })}
-            >
-              {errorMessage}
-            </div>
-            <div className={styles.backToRecipe}>
-              {this.getRecipeID() !== undefined && (
-                <Link
-                  to={`/recipe/${this.getRecipeID()}`}
-                  className={styles.backToRecipeLink}
-                >
-                  <span className={styles.arrow}>{"<<"}</span>Tillbaka till
-                  receptet
-                </Link>
-              )}
-            </div>
-            <form
-              onSubmit={this.handleSubmit}
-              name="addRecipe"
-              className={styles.recipeForm}
-            >
-              <label htmlFor="name">Receptnamn</label>
-              <input
-                id="name"
-                name="name"
-                type="text"
-                value={name}
-                onChange={this.handleChange}
-                className={styles.input}
-              />
-
-              <label htmlFor="ingredients">Ingredienser</label>
-              <textarea
-                cols="50"
-                rows="10"
-                id="ingredients"
-                name="ingredients"
-                type="text"
-                value={ingredients}
-                onChange={this.handleChange}
-                className={styles.textarea}
-              />
-
-              <label htmlFor="description">Beskrivning</label>
-              <textarea
-                cols="50"
-                rows="10"
-                id="description"
-                name="description"
-                type="text"
-                value={description}
-                onChange={this.handleChange}
-                className={styles.textarea}
-              />
-
-              <label htmlFor="time_cooking">Tidsåtgång</label>
-              <input
-                id="time_cooking"
-                name="time_cooking"
-                type="text"
-                defaultValue={recipe ? time_cooking : ""}
-                className={styles.input}
-              />
-
-              <label htmlFor="difficulty">Svårighetsgrad</label>
-              <select
-                id="difficulty"
-                name="difficulty"
-                value={difficulty}
-                onChange={this.handleChange}
-                className={styles.select}
-              >
-                <option value="Lätt">Lätt</option>
-                <option value="Medel">Medel</option>
-                <option value="Svår">Svårt</option>
-              </select>
-
-              <label htmlFor="category">Kategori</label>
-              <select
-                id="categories_id"
-                name="categories_id"
-                value={categories_id}
-                onChange={this.handleChange}
-                className={styles.select}
-              >
-                {this.renderCategoryOption()}
-              </select>
-              <label htmlFor="sub_category_id">Underkategori</label>
-              <select
-                id="sub_category_id"
-                name="sub_category_id"
-                value={sub_category_id}
-                onChange={this.handleChange}
-                className={styles.select}
-              >
-                {this.renderSubCategoryOptions()}
-              </select>
-              {this.getRecipeID() !== undefined ? (
-                <button className={styles.button}>Uppdatera recept</button>
-              ) : (
-                <button className={styles.button}>Lägg till recept</button>
-              )}
-            </form>
+  return (
+    <div className={styles.recipeWrapper}>
+      {checkLoggedIn(user) ? (
+        <div className={styles.formWrapper}>
+          <div
+            className={classnames(styles.errorMessage, {
+              [styles.active]: errorMessage !== ""
+            })}
+          >
+            {errorMessage}
           </div>
-        ) : (
-          <div>
-            <p>Ej Inloggad visa inloggnings formulär</p>
+          <div className={styles.backToRecipe}>
+            {getRecipeID() !== undefined && (
+              <Link
+                to={`/recipe/${getRecipeID()}`}
+                className={styles.backToRecipeLink}
+              >
+                <span className={styles.arrow}>{"<<"}</span>Tillbaka till
+                receptet
+              </Link>
+            )}
           </div>
-        )}
-      </div>
-    )
-  }
+          <form
+            onSubmit={handleSubmit}
+            name="addRecipe"
+            className={styles.recipeForm}
+          >
+            <label htmlFor="name">Receptnamn</label>
+            <input
+              id="name"
+              name="name"
+              type="text"
+              value={recipe.name}
+              onChange={handleChange}
+              className={styles.input}
+            />
+
+            <label htmlFor="ingredients">Ingredienser</label>
+            <textarea
+              cols="50"
+              rows="10"
+              id="ingredients"
+              name="ingredients"
+              type="text"
+              value={recipe.ingredients}
+              onChange={handleChange}
+              className={styles.textarea}
+            />
+
+            <label htmlFor="description">Beskrivning</label>
+            <textarea
+              cols="50"
+              rows="10"
+              id="description"
+              name="description"
+              type="text"
+              value={recipe.description}
+              onChange={handleChange}
+              className={styles.textarea}
+            />
+
+            <label htmlFor="time_cooking">Tidsåtgång</label>
+            <input
+              id="time_cooking"
+              name="time_cooking"
+              type="text"
+              defaultValue={recipe ? recipe.time_cooking : ""}
+              className={styles.input}
+            />
+
+            <label htmlFor="difficulty">Svårighetsgrad</label>
+            <select
+              id="difficulty"
+              name="difficulty"
+              value={recipe.difficulty}
+              onChange={handleChange}
+              className={styles.select}
+            >
+              <option value="Lätt">Lätt</option>
+              <option value="Medel">Medel</option>
+              <option value="Svår">Svårt</option>
+            </select>
+
+            <label htmlFor="category">Kategori</label>
+            <select
+              id="categories_id"
+              name="categories_id"
+              value={recipe.categories_id}
+              onChange={handleChange}
+              className={styles.select}
+            >
+              {renderCategoryOption()}
+            </select>
+            <label htmlFor="sub_category_id">Underkategori</label>
+            <select
+              id="sub_category_id"
+              name="sub_category_id"
+              value={recipe.sub_category_id}
+              onChange={handleChange}
+              className={styles.select}
+            >
+              {renderSubCategoryOptions()}
+            </select>
+            {getRecipeID() !== undefined ? (
+              <button className={styles.button}>Uppdatera recept</button>
+            ) : (
+              <button className={styles.button}>Lägg till recept</button>
+            )}
+          </form>
+        </div>
+      ) : (
+        <div>
+          <p>Ej Inloggad visa inloggnings formulär</p>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default AddRecipe
