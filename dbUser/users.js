6b145feda23b537183a34455c7b0371544e7bb54
jennieann <mail@jennie-ann.se>
2020-11-24 09:13:35 +0100

    Update user

--- dbUser/users.js
+++ dbUser/users.js
@@ -1,39 +1,39 @@
 var records = [
   {
     id: 1,
-    username: 'jennie',
-    password: 'secret',
-    displayName: 'Jennie',
-    emails: [{ value: 'jennie@example.com' }]
+    username: "jennie",
+    password: process.env.PASSWORD,
+    displayName: "Jennie",
+    emails: [{ value: "jennie@example.com" }],
   },
   {
     id: 2,
-    username: 'jill',
-    password: 'birthday',
-    displayName: 'Jill',
-    emails: [{ value: 'jill@example.com' }]
-  }
-];
+    username: "jill",
+    password: process.env.PASSWORD,
+    displayName: "Jill",
+    emails: [{ value: "jill@example.com" }],
+  },
+]
 
-exports.findById = function(id, cb) {
-  process.nextTick(function() {
-    var idx = id - 1;
+exports.findById = function (id, cb) {
+  process.nextTick(function () {
+    var idx = id - 1
     if (records[idx]) {
-      cb(null, records[idx]);
+      cb(null, records[idx])
     } else {
-      cb(new Error('User ' + id + ' does not exist'));
+      cb(new Error("User " + id + " does not exist"))
     }
-  });
-};
+  })
+}
 
-exports.findByUsername = function(username, cb) {
-  process.nextTick(function() {
+exports.findByUsername = function (username, cb) {
+  process.nextTick(function () {
     for (var i = 0, len = records.length; i < len; i++) {
-      var record = records[i];
+      var record = records[i]
       if (record.username === username) {
-        return cb(null, record);
+        return cb(null, record)
       }
     }
-    return cb(null, null);
-  });
-};
+    return cb(null, null)
+  })
+}
