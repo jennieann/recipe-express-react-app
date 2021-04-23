b13f8c74e7d66b6457c2f08d2f6bac1a34816eca
jennieann <mail@jennie-ann.se>
2021-04-23 17:12:37 +0200

    add userEvent  for testing, verify with small test that Jest works.

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,57 +1,16 @@
 import React from "react"
 import "@testing-library/jest-dom/extend-expect"
 import { render, screen } from "@testing-library/react"
+import userEvent from "@testing-library/user-event"
 import { BrowserRouter as Router } from "react-router-dom"
 import App from "./App"
 
-it("renders without crashing", () => {
-  const { container } = render(
+it("renders without crashing", async () => {
+  render(
     <Router>
       <App />
     </Router>
   )
   expect(screen.getByText("Mina recept")).toBeInTheDocument()
-  expect(container).toMatchInlineSnapshot(`
-<div>
-  <div
-    class="App"
-  >
-    <h1
-      class="header"
-    >
-      Mina recept
-    </h1>
-    <nav
-      class="navbar"
-    >
-      <div
-        class="navbar-container"
-      >
-        <a
-          aria-current="page"
-          class="nav-link nav-link-active"
-          href="/"
-        >
-          Hem
-        </a>
-        <a
-          class="nav-link"
-          href="/categories"
-        >
-          Kategorier
-        </a>
-        <a
-          class="nav-link"
-          href="/addRecipe"
-        >
-          Lägg till recept
-        </a>
-      </div>
-    </nav>
-  </div>
-</div>
-`)
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
-  // ReactDOM.unmountComponentAtNode(div);
+  expect(screen.getByRole("link", { name: "Kategorier" })).toBeInTheDocument()
 })
